94d5d833096ccfa6cd0d47b6be440c109af6f9e1
Hannah Høilo <143997275+Hannahhoilo@users.noreply.github.com>
2024-04-18 12:19:20 +0200

    hello

---
+++ nysimon/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import TodoList from './components/TodoList'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <TodoList></TodoList>
+
+    </>
+  )
+}
+
+export default App
---
+++ nysimon/src/components/TodoList.jsx
@@ -0,0 +1,72 @@
+import React, { useReducer, useState } from "react";
+import styles from "./TodoList.module.css";
+
+const TodoList = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "ADD_TODO":
+        return [
+          ...state,
+          {
+            title: action.payload,
+            id: Date.now(),
+            isCompleted: false,
+          },
+        ];
+      case "CHECK":
+        return state.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, isCompleted: !todo.isCompleted }
+            : todo
+        );
+      case "RESET":
+        return [];
+
+      default:
+        return state;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_TODO", payload: inputValue });
+    setInputValue("");
+  };
+
+  const [todos, dispatch] = useReducer(reducer, []);
+
+  return (
+    <>
+      <h2>Todo List</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setInputValue(e.target.value)}
+          type="text"
+          value={inputValue}
+        />
+        <button>Add todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li
+              key={todo.id}
+              className={todo.isCompleted ? styles.todoCompleted : styles.todo}
+            >
+              <input
+                type="checkbox"
+                onChange={() => dispatch({ type: "CHECK", payload: todo.id })}
+              />
+              {todo.title}
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
+    </>
+  );
+};
+
+export default TodoList;
